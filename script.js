13ec0ef47521321986023deb7eadb8e1e09be30d
ammar <131229138+ammar-muslim@users.noreply.github.com>
2024-02-15 12:16:07 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let otherLinks = document.getElementsByClassName("otherLinks")[0]
+
+otherLinks.onclick = () => {
+    let mega = document.getElementsByClassName("mega-menu")[0]
+    mega.classList.toggle('visable')
+}
+
+
+
